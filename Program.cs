96a125975cf7ef987e272f29dc37bf4aa9bc143b
Toomas J. <tom.juri@hotmail.com.>
2024-01-06 21:00:33 +0200

    add week2 code

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿string hello = "  you are the champions?";
+
+int stringLength = hello.Length; 
+
+hello = hello.Trim();
+
+char firstletter = hello[0];
+Console.WriteLine(firstletter);
+
+hello = hello.Replace(hello[0], char.ToUpper(hello[0])).Replace('?', '!');
+
+firstletter = hello[0];
+Console.WriteLine(firstletter);
+
+Console.WriteLine(hello);
+
+
+
+
+
+
+//nt wordCounter = 1;
+
+//string trimmedString = hello.Trim();    
+
+
+
+
+
+
+
+//Console.WriteLine($"Word count in {hello}: {wordCounter} words.");
